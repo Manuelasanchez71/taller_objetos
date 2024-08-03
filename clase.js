782b2f21a9feec2aa5670d3a2dc8885211c43a0c
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-08-03 16:13:41 -0500

    Encapsulamiento - Implementación de Validaciones por [Manuela]

--- clase.js
+++ clase.js
@@ -8,22 +8,40 @@ class Persona {
         this.establecerEdad(edad);
         this.establecerFechaNacimiento(fechaNacimiento);
       }
-    
-      establecerNombre() {
+      establecerNombre(nombre) {
+        if (typeof nombre === 'string' && nombre.trim() !== '') {
+          this.#nombre = nombre;
+        } else {
+          throw new Error('Nombre inválido.');
+        }
       }
     
       obtenerNombre() {
+        return this.#nombre;
       }
     
-      establecerEdad() {
+      establecerEdad(edad) {
+        if (Number.isInteger(edad) && edad >= 0) {
+          this.#edad = edad;
+        } else {
+          throw new Error('Edad inválida.');
+        }
       }
     
       obtenerEdad() {
+        return this.#edad;
       }
     
-      establecerFechaNacimiento() {
-          }
+      establecerFechaNacimiento(fechaNacimiento) {
+        if (fechaNacimiento instanceof Date && !isNaN(fechaNacimiento)) {
+          this.#fechaNacimiento = fechaNacimiento;
+        } else {
+          throw new Error('Fecha de nacimiento inválida.');
+        }
+      }
     
       obtenerFechaNacimiento() {
+        return this.#fechaNacimiento;
       }
+    
     }

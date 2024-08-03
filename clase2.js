ff43f25edb75746ea159e58f1af2c49b3d640652
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-08-03 18:44:42 -0500

    Polimorfismo - Implementación en Clase Estudiante por [Manuela]

--- clase2.js
+++ clase2.js
@@ -1,1 +1,15 @@
-const Estudiante = require("./clase1")
+const Estudiante = require("./clase")
+
+
+class Trabajador {
+    realizarTarea() {
+      throw new Error('Este método debe ser implementado por la subclase');
+    }
+  }
+  
+  class Empleado extends Persona {
+    constructor(nombre, edad, fechaNacimiento, puesto) {
+      super(nombre, edad, fechaNacimiento);
+      this.puesto = puesto;
+    }
+  }  

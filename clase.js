c80e30cccba58fcf61d79845b1f3bcbf41734aa4
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-08-03 17:11:24 -0500

    Encapsulamiento - Implementación de Método adicional calcularFechaNacimiento() por [Nombre]

--- clase.js
+++ clase.js
@@ -49,5 +49,23 @@ class Persona {
         const birthYear = currentYear - this.#edad;
         return new Date(birthYear, 0, 1);
       }
+
+      
     
     }
+
+    // Crear una instancia de la clase Persona
+const persona = new Persona('Manuela Sanchez', 25, new Date(1998, 7, 3));
+
+// Mostrar el nombre, edad y fecha de nacimiento
+console.log(`Nombre: ${persona.obtenerNombre()}`);
+console.log(`Edad: ${persona.obtenerEdad()}`);
+console.log(`Fecha de nacimiento proporcionada: ${persona.obtenerFechaNacimiento().toDateString()}`);
+
+// Calcular y mostrar la fecha de nacimiento calculada
+const fechaCalculada = persona.calcularFechaNacimiento();
+console.log(`Fecha de nacimiento calculada: ${fechaCalculada.toDateString()}`);
+
+
+
+module.exports=Persona;

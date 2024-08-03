1f399f51ce86e3e4d2b4bf9d3769864b5870c07b
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-08-03 17:25:05 -0500

    Herencia - Implementación de Constructores por [Manuela]

--- clase1.js
+++ clase1.js
@@ -1,60 +1,6 @@
 const Persona = require("./clase")
 
-class Persona {
-    #nombre;
-    #edad;
-    #fechaNacimiento;
-  
-    constructor(nombre, edad, fechaNacimiento) {
-      this.establecerNombre(nombre);
-      this.establecerEdad(edad);
-      this.establecerFechaNacimiento(fechaNacimiento);
-    }
-  
-    establecerNombre(nombre) {
-      if (typeof nombre === 'string' && nombre.trim() !== '') {
-        this.#nombre = nombre;
-      } else {
-        throw new Error('Nombre inválido.');
-      }
-    }
-  
-    obtenerNombre() {
-      return this.#nombre;
-    }
-  
-    establecerEdad(edad) {
-      if (Number.isInteger(edad) && edad >= 0) {
-        this.#edad = edad;
-      } else {
-        throw new Error('Edad inválida.');
-      }
-    }
-  
-    obtenerEdad() {
-      return this.#edad;
-    }
-  
-    establecerFechaNacimiento(fechaNacimiento) {
-      if (fechaNacimiento instanceof Date && !isNaN(fechaNacimiento)) {
-        this.#fechaNacimiento = fechaNacimiento;
-      } else {
-        throw new Error('Fecha de nacimiento inválida.');
-      }
-    }
-  
-    obtenerFechaNacimiento() {
-      return this.#fechaNacimiento;
-    }
-  
-    calcularFechaNacimiento() {
-      const currentYear = new Date().getFullYear();
-      const birthYear = currentYear - this.#edad;
-      return new Date(birthYear, 0, 1);
-    }
-  }
-  
-  class Estudiante extends Persona {
+class Estudiante extends Persona {
     constructor(nombre, edad, fechaNacimiento, grado) {
       super(nombre, edad, fechaNacimiento);
       this.grado = grado;
@@ -77,8 +23,6 @@ class Persona {
     }
   }
   
- 
+  
   const estudiante = new Estudiante('Manuela Sanchez', 25, new Date(1998, 7, 3), 'Ingeniería');
   console.log(estudiante.toString());
-  
-  

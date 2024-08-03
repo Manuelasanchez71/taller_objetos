7d38790d40227d20ebdd6ca7f832a2d88a97c8c7
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-08-03 16:11:11 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por [Manuela]

---
+++ clase.js
@@ -0,0 +1,29 @@
+class Persona {
+    #nombre;
+    #edad;
+    #fechaNacimiento;
+
+    constructor(nombre, edad, fechaNacimiento) {
+        this.establecerNombre(nombre);
+        this.establecerEdad(edad);
+        this.establecerFechaNacimiento(fechaNacimiento);
+      }
+    
+      establecerNombre() {
+      }
+    
+      obtenerNombre() {
+      }
+    
+      establecerEdad() {
+      }
+    
+      obtenerEdad() {
+      }
+    
+      establecerFechaNacimiento() {
+          }
+    
+      obtenerFechaNacimiento() {
+      }
+    }

2f40e9e6ade30c97f39df6216ff371031f6bbb1f
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-08-04 13:52:28 -0500

    Herencia y Polimorfismo - Implementación de Subclase Vendedor por [Manuela]

---
+++ vendedor.js
@@ -0,0 +1,20 @@
+const Empleado = require('./empleado');
+
+class Vendedor extends Empleado {
+  constructor(nombre, salario, comision) {
+    super(nombre, salario);
+    this.comision = comision;
+  }
+
+  establecerComision(comision) {
+    if (typeof comision === 'number' && comision >= 0) {
+      this.comision = comision;
+    } else {
+      throw new Error('Comisión inválida.');
+    }
+  }
+
+  obtenerComision() {
+    return this.comision;
+  }
+}
